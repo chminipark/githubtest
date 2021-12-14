46b97d780235ec590a81ee791074f7c19d4f7a76
minii <minii@miniis-Mac.local>
2021-12-14 13:28:18 +0900

    first commit

---
+++ githubtestt/githubtesttApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubtesttApp.swift
+//  githubtestt
+//
+//  Created by minii on 2021/12/14.
+//
+
+import SwiftUI
+
+@main
+struct githubtesttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
